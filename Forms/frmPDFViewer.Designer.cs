f47444b30834b2990d0db218eb9f72b4251075b9
LoSantosYT <ericsancli@gmail.com>
2025-05-06 04:20:09 +0200

    refactor: pdfViewer

--- Forms/frmPDFViewer.Designer.cs
+++ Forms/frmPDFViewer.Designer.cs
@@ -29,20 +29,51 @@ namespace NavesImperiales
         /// </summary>
         private void InitializeComponent()
         {
+            this.pdfViewer1 = new PdfiumViewer.PdfViewer();
+            this.pbClose = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.Location = new System.Drawing.Point(-2, 0);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.ShowToolbar = false;
+            this.pdfViewer1.Size = new System.Drawing.Size(588, 577);
+            this.pdfViewer1.TabIndex = 0;
+            this.pdfViewer1.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitWidth;
+            // 
+            // pbClose
+            // 
+            this.pbClose.Image = global::NavesImperiales.Properties.Resources.delete;
+            this.pbClose.Location = new System.Drawing.Point(550, 597);
+            this.pbClose.Name = "pbClose";
+            this.pbClose.Size = new System.Drawing.Size(20, 21);
+            this.pbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbClose.TabIndex = 13;
+            this.pbClose.TabStop = false;
+            this.pbClose.Click += new System.EventHandler(this.pbClose_Click);
             // 
             // frmPDFViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(582, 630);
+            this.Controls.Add(this.pbClose);
+            this.Controls.Add(this.pdfViewer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmPDFViewer";
             this.Text = "frmPDFViewer";
             this.Load += new System.EventHandler(this.frmPDFViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PdfiumViewer.PdfViewer pdfViewer1;
+        private System.Windows.Forms.PictureBox pbClose;
     }
 }
--- Forms/frmPDFViewer.cs
+++ Forms/frmPDFViewer.cs
@@ -6,7 +6,6 @@ namespace NavesImperiales
 {
     public partial class frmPDFViewer : Form
     {
-        private PdfViewer pdfViewer;
         private string pdfPath;
 
         // Constructor que recibe la ruta del PDF
@@ -20,19 +19,20 @@ namespace NavesImperiales
 
         private void frmPDFViewer_Load(object sender, EventArgs e)
         {
-            pdfViewer = new PdfViewer();
-            pdfViewer.Dock = DockStyle.Fill;
-            this.Controls.Add(pdfViewer);
-
             try
             {
                 var document = PdfDocument.Load(pdfPath);
-                pdfViewer.Document = document;
+                pdfViewer1.Document = document;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("No se pudo cargar el PDF: " + ex.Message);
             }
+        }
+
+        private void pbClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
