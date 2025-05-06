8f57da3f9e1bdeaf9f6e98f326e48130ab3dbc43
LoSantosYT <ericsancli@gmail.com>
2025-05-06 03:48:00 +0200

    feat: add title,description and ships images

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -30,32 +30,45 @@ namespace NavesImperiales
         private void InitializeComponent()
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.lvNaus = new System.Windows.Forms.ListView();
             this.dgvEspec = new System.Windows.Forms.DataGridView();
             this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colValor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pbFilePDF = new System.Windows.Forms.PictureBox();
             this.pbPlanols = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.RichTextBox();
+            this.wmpView360 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pbSelectedImg = new System.Windows.Forms.PictureBox();
+            this.lvShipsImages = new System.Windows.Forms.ListView();
+            this.lbShipName = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEspec)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbFilePDF)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlanols)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpView360)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelectedImg)).BeginInit();
             this.SuspendLayout();
             // 
             // lvNaus
             // 
             this.lvNaus.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lvNaus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvNaus.ForeColor = System.Drawing.SystemColors.Window;
             this.lvNaus.HideSelection = false;
-            this.lvNaus.Location = new System.Drawing.Point(12, 12);
+            this.lvNaus.Location = new System.Drawing.Point(29, 41);
             this.lvNaus.Name = "lvNaus";
-            this.lvNaus.Size = new System.Drawing.Size(250, 376);
+            this.lvNaus.Size = new System.Drawing.Size(196, 216);
             this.lvNaus.TabIndex = 0;
             this.lvNaus.UseCompatibleStateImageBehavior = false;
+            this.lvNaus.View = System.Windows.Forms.View.List;
             this.lvNaus.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
             // 
             // dgvEspec
             // 
             this.dgvEspec.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dgvEspec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEspec.ColumnHeadersVisible = false;
             this.dgvEspec.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colNombre,
             this.colValor});
@@ -69,13 +82,13 @@ namespace NavesImperiales
             this.dgvEspec.DefaultCellStyle = dataGridViewCellStyle1;
             this.dgvEspec.Enabled = false;
             this.dgvEspec.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.dgvEspec.Location = new System.Drawing.Point(280, 270);
+            this.dgvEspec.Location = new System.Drawing.Point(298, 427);
             this.dgvEspec.Name = "dgvEspec";
             this.dgvEspec.ReadOnly = true;
             this.dgvEspec.RowHeadersVisible = false;
             this.dgvEspec.RowHeadersWidth = 62;
             this.dgvEspec.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvEspec.Size = new System.Drawing.Size(341, 242);
+            this.dgvEspec.Size = new System.Drawing.Size(316, 209);
             this.dgvEspec.TabIndex = 2;
             // 
             // colNombre
@@ -98,7 +111,7 @@ namespace NavesImperiales
             // 
             this.pbFilePDF.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.pbFilePDF.Image = global::NavesImperiales.Properties.Resources.pdfW;
-            this.pbFilePDF.Location = new System.Drawing.Point(620, 279);
+            this.pbFilePDF.Location = new System.Drawing.Point(665, 427);
             this.pbFilePDF.Name = "pbFilePDF";
             this.pbFilePDF.Size = new System.Drawing.Size(60, 63);
             this.pbFilePDF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -108,17 +121,92 @@ namespace NavesImperiales
             // 
             // pbPlanols
             // 
-            this.pbPlanols.Location = new System.Drawing.Point(280, 12);
+            this.pbPlanols.Location = new System.Drawing.Point(240, 41);
             this.pbPlanols.Name = "pbPlanols";
-            this.pbPlanols.Size = new System.Drawing.Size(400, 250);
-            this.pbPlanols.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPlanols.Size = new System.Drawing.Size(485, 370);
+            this.pbPlanols.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbPlanols.TabIndex = 1;
             this.pbPlanols.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Snow;
+            this.lblTitle.Location = new System.Drawing.Point(25, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(185, 24);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Light Fighter Ships";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtDescription.Location = new System.Drawing.Point(29, 263);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(196, 284);
+            this.txtDescription.TabIndex = 7;
+            this.txtDescription.Text = "";
+            // 
+            // wmpView360
+            // 
+            this.wmpView360.Enabled = true;
+            this.wmpView360.Location = new System.Drawing.Point(775, 41);
+            this.wmpView360.Name = "wmpView360";
+            this.wmpView360.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpView360.OcxState")));
+            this.wmpView360.Size = new System.Drawing.Size(592, 370);
+            this.wmpView360.TabIndex = 9;
+            this.wmpView360.Visible = false;
+            // 
+            // pbSelectedImg
+            // 
+            this.pbSelectedImg.Location = new System.Drawing.Point(775, 41);
+            this.pbSelectedImg.Name = "pbSelectedImg";
+            this.pbSelectedImg.Size = new System.Drawing.Size(592, 370);
+            this.pbSelectedImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSelectedImg.TabIndex = 8;
+            this.pbSelectedImg.TabStop = false;
+            this.pbSelectedImg.Visible = false;
+            // 
+            // lvShipsImages
+            // 
+            this.lvShipsImages.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lvShipsImages.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvShipsImages.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvShipsImages.ForeColor = System.Drawing.SystemColors.Window;
+            this.lvShipsImages.HideSelection = false;
+            this.lvShipsImages.Location = new System.Drawing.Point(759, 427);
+            this.lvShipsImages.Name = "lvShipsImages";
+            this.lvShipsImages.Size = new System.Drawing.Size(647, 100);
+            this.lvShipsImages.TabIndex = 10;
+            this.lvShipsImages.UseCompatibleStateImageBehavior = false;
+            this.lvShipsImages.SelectedIndexChanged += new System.EventHandler(this.lvShipsImages_SelectedIndexChanged);
+            // 
+            // lbShipName
+            // 
+            this.lbShipName.AutoSize = true;
+            this.lbShipName.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbShipName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbShipName.ForeColor = System.Drawing.Color.Snow;
+            this.lbShipName.Location = new System.Drawing.Point(967, 522);
+            this.lbShipName.Name = "lbShipName";
+            this.lbShipName.Size = new System.Drawing.Size(0, 25);
+            this.lbShipName.TabIndex = 11;
             // 
             // MainForm
             // 
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1036, 660);
+            this.ClientSize = new System.Drawing.Size(1434, 660);
+            this.Controls.Add(this.lbShipName);
+            this.Controls.Add(this.lvShipsImages);
+            this.Controls.Add(this.wmpView360);
+            this.Controls.Add(this.pbSelectedImg);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.pbFilePDF);
             this.Controls.Add(this.dgvEspec);
             this.Controls.Add(this.pbPlanols);
@@ -128,7 +216,10 @@ namespace NavesImperiales
             ((System.ComponentModel.ISupportInitialize)(this.dgvEspec)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbFilePDF)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlanols)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpView360)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelectedImg)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -140,5 +231,11 @@ namespace NavesImperiales
         private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn colValor;
         private System.Windows.Forms.PictureBox pbFilePDF;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.RichTextBox txtDescription;
+        private AxWMPLib.AxWindowsMediaPlayer wmpView360;
+        private System.Windows.Forms.PictureBox pbSelectedImg;
+        private System.Windows.Forms.ListView lvShipsImages;
+        private System.Windows.Forms.Label lbShipName;
     }
 }
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -19,6 +19,8 @@ namespace NavesImperiales
         private InfoDetail selectedDetail;
 
         private string basePath = Application.StartupPath;
+        private List<string> imagePaths = new List<string>();
+
 
         public MainForm()
         {
@@ -40,6 +42,70 @@ namespace NavesImperiales
                 MessageBox.Show("No se encontró el PDF.");
             }
         }
+        private void showMediaSelectedImg (int index)
+        {
+            if ( index >= 0 && index < 4)
+            {
+                wmpView360.Visible = false;
+                pbSelectedImg.Visible = true;
+
+                string selectedImagePath = imagePaths[index];
+                pbSelectedImg.Image = Image.FromFile(selectedImagePath);
+            } else
+            {
+                wmpView360.Visible = true;
+                wmpView360.uiMode = "none";
+                pbSelectedImg.Visible = false;
+
+                string videoPath = Path.Combine(basePath, "images/", selectedOption.textOption);
+                videoPath = Directory.GetFiles(videoPath, "*.mp4")[0];
+                wmpView360.URL = videoPath;
+                wmpView360.stretchToFit = true;
+            }
+
+        }
+
+        private void loadShipsImages()
+        {
+            string[] images = new string[]
+            {
+                selectedDetail.FrontView,
+                selectedDetail.SideView,
+                selectedDetail.TopView,
+                selectedDetail.RearView,
+                selectedDetail.View360
+            };
+
+            ImageList imgList = new ImageList();
+            imgList.ImageSize = new Size(64, 64); // Tamaño más grande para que se vean bien
+
+            lvShipsImages.Clear(); // Limpiar ítems anteriores
+            lvShipsImages.View = View.LargeIcon;
+            lvShipsImages.LargeImageList = imgList;
+            
+            imagePaths.Clear();
+
+
+            int index = 0;
+            foreach (var imgPath in images)
+            {
+                if (!string.IsNullOrEmpty(imgPath))
+                {
+                    string iconPath = Path.Combine(basePath, "images/", selectedOption.textOption, imgPath);
+                    imagePaths.Add(iconPath);
+                    if (File.Exists(iconPath))
+                    {
+                        imgList.Images.Add(Image.FromFile(iconPath));
+
+                        // Agregar el ítem con índice correspondiente
+                        ListViewItem item = new ListViewItem("", index);
+                        lvShipsImages.Items.Add(item);
+                        index++;
+                    }
+                }
+            }
+        }
+
 
         private void listView_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -55,11 +121,14 @@ namespace NavesImperiales
             if (selectedDetail == null) return;
 
             // Mostrar imagen principal (Blueprint)
-            string imgPath = Path.Combine(basePath, "images", selectedDetail.Blueprint);
+            string imgPath = Path.Combine(basePath, "images/", selectedOption.textOption, selectedDetail.Blueprint);
             if (File.Exists(imgPath))
                 pbPlanols.Image = Image.FromFile(imgPath);
             else
                 pbPlanols.Image = null;
+
+            lbShipName.Text = selectedDetail.title;
+            txtDescription.Text = selectedDetail.textInfoDetail;
 
             // Mostrar detalles en el DataGridView
             dgvEspec.Rows.Clear();
@@ -73,6 +142,7 @@ namespace NavesImperiales
                 dgvEspec.Rows.Add("Armament", selectedDetail.Data.Armament);
             }
             dgvEspec.ClearSelection();
+            loadShipsImages();
         }
 
         private void Load_Data()
@@ -81,7 +151,6 @@ namespace NavesImperiales
             technicalInfo = XmlManager<TechnicalInfo>.Load(xmlPath);
 
             // Configurar el ListView
-            lvNaus.View = View.Details;  // Establecer la vista a "Detalles"
             lvNaus.Columns.Add("Ship Name", -2, HorizontalAlignment.Left);  // Añadir solo una columna para el nombre de la nave
 
             // Crear una lista de imágenes
@@ -102,7 +171,9 @@ namespace NavesImperiales
                 }
             }
 
-            lvNaus.LargeImageList = imgList;  // Asignar la lista de imágenes al ListView
+            lvNaus.View = View.List;
+            lvNaus.SmallImageList = imgList; // 🔑 esta línea es necesaria
+                                             // Asignar la lista de imágenes al ListView
 
             // Añadir ítems al ListView
             foreach (var option in technicalInfo.InfoOptions)
@@ -110,6 +181,18 @@ namespace NavesImperiales
                 var item = new ListViewItem(option.textOption);
                 item.ImageKey = option.textOption;  // Asegurarse de que el ImageKey coincide con el que tenemos en el ImageList
                 lvNaus.Items.Add(item);  // Agregar el ítem al ListView
+            }
+        }
+
+        private void lvShipsImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Verificar si hay algún ítem seleccionado
+            if (lvShipsImages.SelectedIndices.Count > 0)
+            {
+                // Obtener el índice del ítem seleccionado
+                int selectedIndex = lvShipsImages.SelectedItems[0].ImageIndex;
+                showMediaSelectedImg(selectedIndex);
+
             }
         }
     }
